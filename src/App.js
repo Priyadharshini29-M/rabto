e90caf1ead1316aa2d9800e756fbe6cbdebf592e
vicky <srivicky2000@gmail.com>
2022-06-11 11:02:20 +0530

    working on admin routes

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Home from "./dummy/screens/Home"
 import {BrowserRouter as Router, Routes, Route} from "react-router-dom"
 import Profile from './dummy/screens/Profile';
+import Admin from './dummy/screens/Admin';
 
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
       <Routes>
         <Route path='/:username' element={<Profile/>} />
         <Route path='/' element={<Home/>} />
+        <Route path='/admin' element={<Admin/>} />
       </Routes>
     </Router>
   );
---
+++ src/dummy/screens/Admin.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Admin() {
+  return (
+    <div>
+        Admin
+        <div>
+            <p>
+                Here Admin 
+            </p>
+        </div>
+    </div>
+  )
+}
+
+export default Admin
