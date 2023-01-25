73a37ab962ef6dba20834016975ad7fbcad62013
Varun <nurav2037@gmial.com>
2023-01-25 15:08:05 +0530

    working on: ui changes

--- src/dummy/data/users.js
+++ src/dummy/data/users.js
@@ -1,239 +1,16 @@
- export const userData = [{
-    userBio: {
-        name: "DJ Sri Vigneshwar",
-        userName: "vicky",
-        position: "MD @goby.in",
-        bio: "theDottest",
-        email: "testtheDot@gmail.com",
-        age: "22",
-        resumeurl: "www.google.com",
-        profile: "https://images.unsplash.com/photo-1529626455594-4ff0802cfb7e?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=60&raw_url=true&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8Z2lybHxlbnwwfHwwfHw%3D&auto=format",
-        links: {
-            instagram: "instagram/varun_ft",
-            fb: "facebook/varun",
-            dribble: "dribble/varun_ft",
-            behance: "behance/varun",
-            linkedin: "linkedin/varun_ft",
-            twitter: "twitter/varun"
-        },
-    },
-    userLinks: [{
-            title: "Portfolio",
-            links: [{
-                    heading: "Aagaram Restaurant",
-                    description: "Unit of Sree Catering presents Aagaaram restaurant was our Phenomenal client .",
-                    url: "https://rbsvirundhagam.in/",
-                    image: "imagetest"
-                },
-                {
-                    heading: "Coding for Deaf",
-                    description: "We are an organisation dedicated to involving those who are hard of hearing in programming and working with arduino, as they are underrepresented in this field.",
-                    url: "https://www.brksystems.com/#",
-                    image: "imagetest"
-                },
-                {
-                    heading: "SM Exports Erode",
-                    description: "We have made a niche for ourselves by fulfilling commodity requirements in India, Middle East, Sri Lanka , Malaysia",
-                    url: "https://smexportserode.com/",
-                    image: "imagetest"
-                }
-            ]
-        },
-        {
-            title: "Education",
-            links: [{
-                    institutionName: "Park Global International School",
-                    year: "2016 - 2018",
-                    institutionurl: "https://rbsvirundhagam.in/",
-                    grade: "A plus ultra Max"
-                },
-                {
-                    institutionName: "Sri Krishna Engineering collage",
-                    year: "2018 - 2022",
-                    institutionurl: "https://rbsvirundhagam.in/",
-                    grade: "Gold medalist"
-                }
-            ],
-        },
-        {
-            title: "Certifications",
-            links: [{
-                    courseName: "Full Stack Pro Developer",
-                    yoc: "2021",
-                    certificateLink: "https://rbsvirundhagam.in/",
-                    rateYourSkill: "*****"
-                },
-                {
-                    courseName: "Proficiancy with Quantum Computers",
-                    yoc: "2020",
-                    certificateLink: "https://rbsvirundhagam.in/",
-                    rateYourSkill: "*****"
-                }
-            ],
-        },
 
-    ]
-}, {
-    userBio: {
-        name: "Surinther Raja",
-        userName: "suren",
-        position: "CEO @goby.in",
-        bio: "A growth hack company which helps your business grow dif",
-        email: "testtheDot@gmail.com",
-        age: "21",
-        resumeurl: "www.google.com",
-        profile: "https://firebasestorage.googleapis.com/v0/b/gobyin-test-v1.appspot.com/o/WhatsApp%20Image%202022-05-26%20at%209.53.57%20PM.jpeg?alt=media&token=cd33be12-e1ef-4f12-9a8b-dc839744be60",
-        links: {
-            instagram: "https://www.instagram.com/thedot_tech/",
-            fb: "https://www.facebook.com/profile.php?id=100078201942993",
-            dribble: "https://dribbble.com/surintherraja",
-            behance: "https://www.behance.net/warrenpeaceweb",
-            linkedin: "https://www.linkedin.com/in/surinther-raja-s-7370b1125/",
-            twitter: "https://twitter.com/theDot_tech"
-        },
-    },
-    userLinks: [{
-            title: "Portfolio",
-            links: [{
-                    heading: "Aagaram Restaurant",
-                    description: "Unit of Sree Catering presents Aagaaram restaurant was our Phenomenal client .",
-                    url: "https://rbsvirundhagam.in/",
-                    image: "https://firebasestorage.googleapis.com/v0/b/gobyin-test-v1.appspot.com/o/Aagaaram%202.png?alt=media&token=02cfabfa-6c62-4d46-a35d-6855b12c556fst"
-                },
-                {
-                    heading: "Coding for Deaf",
-                    description: "We are an organisation dedicated to involving those who are hard of hearing in programming and working with arduino, as they are underrepresented in this field.",
-                    url: "https://www.brksystems.com/",
-                    image: "https://firebasestorage.googleapis.com/v0/b/gobyin-test-v1.appspot.com/o/Group%208.png?alt=media&token=f0305eeb-66e9-4b3c-97d1-6700c0386f7f"
-                },
-                {
-                    heading: "SM Exports Erode",
-                    description: "We have made a niche for ourselves by fulfilling commodity requirements in India, Middle East, Sri Lanka , Malaysia",
-                    url: "https://smexportserode.com/",
-                    image: "https://firebasestorage.googleapis.com/v0/b/gobyin-test-v1.appspot.com/o/Group%2042.png?alt=media&token=a38d8ece-aeeb-4c2b-9c52-00c1865bd38c"
-                }
-            ]
-        },
-        {
-            title: "Education",
-            links: [{
-                    institutionName: "Amrita Vidyalayam School",
-                    year: "2016 - 2018",
-                    institutionurl: "https://www.eduvidya.com/Schools/Amrita-Vidyalayam,-Nallampalayam-Rd",
-                    grade: "80%",
-                    photo: "https://firebasestorage.googleapis.com/v0/b/gobyin-test-v1.appspot.com/o/Group%20513.png?alt=media&token=5cdcf223-f6ed-4a59-b305-856aa5a4f98e"
-                },
-                {
-                    institutionName: "Jain (Deemed-to-be University)",
-                    year: "2018 - 2022",
-                    institutionurl: "https://www.jainuniversity.ac.in/",
-                    grade: "87%",
-                    photo: "https://firebasestorage.googleapis.com/v0/b/gobyin-test-v1.appspot.com/o/Group%20512.png?alt=media&token=bef975b9-27a4-472a-b889-6126cac170fe"
-                }
-            ],
-        },
-        {
-            title: "Certifications",
-            links: [{
-                    courseName: "Full Stack Pro Developer",
-                    yoc: "2021",
-                    certificateLink: "https://firebasestorage.googleapis.com/v0/b/gobyin-test-v1.appspot.com/o/rn.png?alt=media&token=4c52250c-4c80-42f5-b010-b261d2fe62bc",
-                    rateYourSkill: "4.5"
-                },
-                {
-                    courseName: "MERN Developer",
-                    yoc: "2020",
-                    certificateLink: "https://firebasestorage.googleapis.com/v0/b/gobyin-test-v1.appspot.com/o/mern.png?alt=media&token=065df719-0354-4e1e-8b70-7dede19b925a",
-                    rateYourSkill: "4.8"
-                }
-            ],
-        },
-
-    ]
-}]
+import bangs from "../../assets/images/bangs.webp"
+import hworld from "../../assets/images/hworld.webp"
+import nannir from "../../assets/images/nannir.webp"
+import cbe from "../../assets/images/cbe.webp"
 
 export const buserData = [{
     userBio: {
-        name: "DJ Sri Vigneshwar",
-        userName: "vicky",
-        position: "MD @goby.in",
-        bio: "theDottest",
-        email: "testtheDot@gmail.com",
-        age: "22",
-        resumeurl: "www.google.com",
-        profile: "https://images.unsplash.com/photo-1529626455594-4ff0802cfb7e?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=60&raw_url=true&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8Z2lybHxlbnwwfHwwfHw%3D&auto=format",
-        links: {
-            instagram: "instagram/varun_ft",
-            fb: "facebook/varun",
-            dribble: "dribble/varun_ft",
-            behance: "behance/varun",
-            linkedin: "linkedin/varun_ft",
-            twitter: "twitter/varun"
-        },
-    },
-    userLinks: [{
-            title: "Portfolio",
-            links: [{
-                    heading: "Aagaram Restaurant",
-                    description: "Unit of Sree Catering presents Aagaaram restaurant was our Phenomenal client .",
-                    url: "https://rbsvirundhagam.in/",
-                    image: "imagetest"
-                },
-                {
-                    heading: "Coding for Deaf",
-                    description: "We are an organisation dedicated to involving those who are hard of hearing in programming and working with arduino, as they are underrepresented in this field.",
-                    url: "https://www.brksystems.com/#",
-                    image: "imagetest"
-                },
-                {
-                    heading: "SM Exports Erode",
-                    description: "We have made a niche for ourselves by fulfilling commodity requirements in India, Middle East, Sri Lanka , Malaysia",
-                    url: "https://smexportserode.com/",
-                    image: "imagetest"
-                }
-            ]
-        },
-        {
-            title: "Education",
-            links: [{
-                    institutionName: "Park Global International School",
-                    year: "2016 - 2018",
-                    institutionurl: "https://rbsvirundhagam.in/",
-                    grade: "A plus ultra Max"
-                },
-                {
-                    institutionName: "Sri Krishna Engineering collage",
-                    year: "2018 - 2022",
-                    institutionurl: "https://rbsvirundhagam.in/",
-                    grade: "Gold medalist"
-                }
-            ],
-        },
-        {
-            title: "Certifications",
-            links: [{
-                    courseName: "Full Stack Pro Developer",
-                    yoc: "2021",
-                    certificateLink: "https://rbsvirundhagam.in/",
-                    rateYourSkill: "*****"
-                },
-                {
-                    courseName: "Proficiancy with Quantum Computers",
-                    yoc: "2020",
-                    certificateLink: "https://rbsvirundhagam.in/",
-                    rateYourSkill: "*****"
-                }
-            ],
-        },
-
-    ]
-}, {
-    userBio: {
-        name: "Surinther Raja",
-        userName: "suren",
+        name: "theDot Tech",
+        userName: "thedottech",
         whatsappNumber: "9845318077",
         position: "CEO @goby.in",
-        bio: "Loud Introvert with big mission Fiercely local but available worldwide",
+        bio: "A Growth Hack Company which makes your business grow in digital world.",
         email: "testtheDot@gmail.com",
         age: "21",
         resumeurl: "www.google.com",
@@ -248,61 +25,34 @@ export const buserData = [{
         },
     },
     userLinks: [{
-            title: "Portfolio",
+            id: 1,
+            title: "Social Media Marketing",
             links: [{
-                    heading: "Aagaram Restaurant",
-                    description: "Unit of Sree Catering presents Aagaaram restaurant was our Phenomenal client .",
-                    url: "https://rbsvirundhagam.in/",
-                    image: "https://firebasestorage.googleapis.com/v0/b/gobyin-test-v1.appspot.com/o/Aagaaram%202.png?alt=media&token=02cfabfa-6c62-4d46-a35d-6855b12c556fst"
+                    heading: "Bangs",
+                    description: "Elevating A Luxury Beauty Salon's specialised services in Social Media Platform.",
+                    url: "https://instagram.com/bangssalon_rspuram?igshid=YmMyMTA2M2Y=",
+                    image: bangs
                 },
                 {
-                    heading: "Coding for Deaf",
-                    description: "We are an organisation dedicated to involving those who are hard of hearing in programming and working with arduino, as they are underrepresented in this field.",
-                    url: "https://www.brksystems.com/",
-                    image: "https://firebasestorage.googleapis.com/v0/b/gobyin-test-v1.appspot.com/o/Group%208.png?alt=media&token=f0305eeb-66e9-4b3c-97d1-6700c0386f7f"
+                    heading: "The H World",
+                    description: "Crafting a Mens Herbal Hair Care brand in the social media space with great branding and unique identity.",
+                    url: "https://instagram.com/the_h_world_india?igshid=YmMyMTA2M2Y=",
+                    image: hworld
                 },
                 {
-                    heading: "SM Exports Erode",
-                    description: "We have made a niche for ourselves by fulfilling commodity requirements in India, Middle East, Sri Lanka , Malaysia",
-                    url: "https://smexportserode.com/",
-                    image: "https://firebasestorage.googleapis.com/v0/b/gobyin-test-v1.appspot.com/o/Group%2042.png?alt=media&token=a38d8ece-aeeb-4c2b-9c52-00c1865bd38c"
+                    heading: "Coimbatore Biriyani Eatery",
+                    description: "Developing a restaurant's social media profile and a strong online reputation to captivate the audience.",
+                    url: "https://instagram.com/cbe_coimbatore_briyani_eatery?igshid=YmMyMTA2M2Y=",
+                    image: cbe
+                },
+                {
+                    heading: "Nannir",
+                    description: "Dealing an agricultural patent product company to drive business and boost conversions across PAN India.",
+                    url: "https://instagram.com/nannir_energizer?igshid=YmMyMTA2M2Y=",
+                    image: nannir
                 }
             ]
-        },
-        {
-            title: "Education",
-            links: [{
-                    institutionName: "Amrita Vidyalayam School",
-                    year: "2016 - 2018",
-                    institutionurl: "https://www.eduvidya.com/Schools/Amrita-Vidyalayam,-Nallampalayam-Rd",
-                    grade: "80%",
-                    photo: "https://firebasestorage.googleapis.com/v0/b/gobyin-test-v1.appspot.com/o/Group%20513.png?alt=media&token=5cdcf223-f6ed-4a59-b305-856aa5a4f98e"
-                },
-                {
-                    institutionName: "Jain (Deemed-to-be University)",
-                    year: "2018 - 2022",
-                    institutionurl: "https://www.jainuniversity.ac.in/",
-                    grade: "87%",
-                    photo: "https://firebasestorage.googleapis.com/v0/b/gobyin-test-v1.appspot.com/o/Group%20512.png?alt=media&token=bef975b9-27a4-472a-b889-6126cac170fe"
-                }
-            ],
-        },
-        {
-            title: "Certifications",
-            links: [{
-                    courseName: "Full Stack Pro Developer",
-                    yoc: "2021",
-                    certificateLink: "https://firebasestorage.googleapis.com/v0/b/gobyin-test-v1.appspot.com/o/rn.png?alt=media&token=4c52250c-4c80-42f5-b010-b261d2fe62bc",
-                    rateYourSkill: "4.5"
-                },
-                {
-                    courseName: "MERN Developer",
-                    yoc: "2020",
-                    certificateLink: "https://firebasestorage.googleapis.com/v0/b/gobyin-test-v1.appspot.com/o/mern.png?alt=media&token=065df719-0354-4e1e-8b70-7dede19b925a",
-                    rateYourSkill: "4.8"
-                }
-            ],
-        },
+        }
 
     ]
 }]
