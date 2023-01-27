e4b6c0c9891723fc4ba7afa11c1e5ca5cfab8e35
vicky <srivicky2000@gmail.com>
2023-01-28 02:15:07 +0530

    workin on: deploy one

--- src/dummy/screens/Profile.js
+++ src/dummy/screens/Profile.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { useParams} from 'react-router-dom';
 import Busername from '../components/Busername';
 import Username from '../components/Username';
-
+import {userData} from '.././data/users'
 import { buserData } from '../data/users';
 import { useEffect, useState } from 'react';
 import axios from 'axios';
@@ -31,15 +31,15 @@ function Profile() {
 
 
     const userFilterData = buserData.filter(i => i.userBio.userName === username);
-    const userFilterData1 = userData.filter(i => i.userBio.userName === username);
+    // const userFilterData1 = userData.filter(i => i.userBio.userName === username);
     if(userFilterData[0])
     return (
             <Busername data={userFilterData[0]}/>
     )
-    else if(userFilterData1[0])
-    return (
-        <Username data={userFilterData1[0]} />
-    )
+    // else if(userFilterData1[0])
+    // return (
+    //     <Username data={userFilterData1[0]} />
+    // )
     else
     return (
         <div>
